7e59f66687d13c1c05072b5ea20d0216d9919aec
Maxsuel Oliveira <maxsuelo53@gmail.com>
2022-11-07 15:33:57 -0300

    teste deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node Back-end/index.js & npm start
---
+++ index.js
@@ -0,0 +1,14 @@
+const chalk = require('chalk');
+var express = require('express')
+var app = express()
+const port = 3000;
+
+app.get('/', function (req, res) {
+  res.send('Hello World, teste de deploy automático!')
+})
+
+
+app.listen(port, function () {
+  console.log(chalk.green("Running at Port " +  port  ));
+})
+
